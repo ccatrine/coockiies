48d14588604db9c7c111b3727e2c5abc6a2863b2
ccatrine <153758426+ccatrine@users.noreply.github.com>
2024-03-27 17:08:03 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,43 @@
 'use strict'
+const sliderDivRate=480/1920
 let sliderDiv=document.querySelector("#slider")
 let sliderImages=sliderDiv.querySelectorAll("img")
 let sliderSpeed=1000
-let sliderChangeSpeed=800
+let sliderChangeSpeed=600
+let sliderChangeState=1/sliderChangeSpeed
+let sliderHeight=480
 let currentSlide=0
+let nextSlide=1
+let nextSlideOpacity=0
 let slidesNumber=sliderImages.length
-function nextSlide(){   
-    sliderImages[currentSlide].style.opacity=0
-    currentSlide++
-    if (currentSlide===slidesNumber) {currentSlide=0}
-    sliderImages[currentSlide].style.opacity=1
+function startShowNextSlide(){   
+    sliderImages[currentSlide].style.zIndex=1
+    sliderImages[nextSlide].style.zIndex=2
+  showNextSlide()
+}
+function showNextSlide(){
+    nextSlideOpacity+=sliderChangeState
+    sliderImages[nextSlide].style.opacity=nextSlideOpacity
+    if ( sliderImages[nextSlide].style.opacity<1 ){
+        setTimeout(showNextSlide,1)
+
+    } else {
+        sliderImages[currentSlide].style.opacity=0
+        sliderImages [currentSlide].style.zIndex=0
+        currentSlide++
+        if (currentSlide===slidesNumber) {currentSlide=0}
+        nextSlide=currentSlide+1  
+     if (nextSlide===slidesNumber) {nextSlide=0}
+     nextSlideOpacity=0
+     setTimeout(startShowNextSlide,sliderSpeed)
+    }
 }
 
 
-setInterval(nextSlide,sliderSpeed)
-
-
+setTimeout(startShowNextSlide,sliderSpeed)
+onresize=changeScreenSize
+function changeScreenSize(){
+    sliderDiv.style.height=sliderDiv.offsetWidth*sliderDivRate+"px"
+    console.log(sliderDiv.offsetWidth)
+}
+changeScreenSize()
